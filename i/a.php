f31400f9aa08d5f671831e2652110f7dd72b8f83
66010914106 <66010914106@msu.ac.th>
2026-02-10 09:56:59 +0700

    Add files via upload

---
+++ i/a.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>นัธวุฒิ บัวงาม (มาร์ค)</title>
+</head>
+
+<body>
+
+
+<h1>งาน i</h1>
+<h1>นัธวุฒิ บัวงาม (มาร์ค)</h1>
+
+</body>
+</html>
