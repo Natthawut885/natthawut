9c9d34511a37ce09b0a189a8749dd52ee0ecad1a
66010914106 <66010914106@msu.ac.th>
2026-02-17 12:24:51 +0700

    Delete j directory

--- j/connectdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-	   $host = "localhost";
-	   $user = "root";
-	   $pwd = "12345678";
-	   $db = "4106db";
-	   $conn = mysqli_connect( $host,$user,$pwd,$db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
-	   mysqli_query($conn, "SET NAMES utf8");
-?>   
-
